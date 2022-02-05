69568b99c7369a24be9d8efb1856d5b028ed01ef
LuksFay <luksfaydev@gmail.com>
2022-02-05 17:12:21 -0300

    router con extensión de archivo

--- src/app.jsx
+++ src/app.jsx
@@ -6,21 +6,21 @@ import {
   Redirect,
   Switch
 } from 'react-router-dom';
-import About from './pages/About/About'
-import Home from './pages/Home/Home'
+import About from './pages/About/About.jsx'
+import Home from './pages/Home/Home.jsx'
 
 //import './app.css'; 
 
 
 const App = ()=>{	
     return (
-     <Router>
+    <Router>
        <main>
-         <Route path="/" exact>
-           <Home/> 
-         </Route>
           <Route path="/about">
            <About/> 
+         </Route>
+          <Route path="/">
+           <Home/> 
          </Route>
        </main>
      </Router>
